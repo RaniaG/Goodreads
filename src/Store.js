4a1061f48d7bbc3b128acfd9547a9a2d89e42bd8
shery <sher@gmai.com>
2019-04-26 00:24:49 +0200

    add, edit books

--- src/Store.js
+++ src/Store.js
@@ -8,8 +8,8 @@ export const Store = createStore(Reducer, {
         name: 'Rania',
         email: 'raniagml12@gmail.com',
         photo: 'https://cdn3.vectorstock.com/i/1000x1000/27/92/cartoon-bookworm-vector-5102792.jpg',
-        type: 'user'
-        // type: 'admin'
+        // type: 'user'
+        type: 'admin'
     }
 })
 
